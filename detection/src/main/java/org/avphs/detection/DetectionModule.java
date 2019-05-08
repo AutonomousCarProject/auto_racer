7b2e9c9173e13885822304976b828134ffe8cb3a
Rewind <sploing1@gmail.com>
2019-05-08 10:49:22 -0700

    Updated gitignore and maven stuff

--- detection/src/main/java/org/avphs/detection/DetectionModule.java
+++ detection/src/main/java/org/avphs/detection/DetectionModule.java
@@ -3,6 +3,7 @@ package org.avphs.detection;
 import org.avphs.core.CarModule;
 
 public class DetectionModule implements CarModule {
+
     @Override
     public void update() {
         System.out.println("Detection");
