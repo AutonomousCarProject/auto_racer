518afb0008813a93123f0cb6a48f5a08e96ec342
Kevin <kevindpaulsen@gmail.com>
2019-07-05 14:03:41 -0700

    Updated to gradle.

---
+++ image/src/main/java/org.avphs.image/org/avphs/image/ImageModule.java
@@ -0,0 +1,10 @@
+package org.avphs.image;
+
+import org.avphs.core.CarModule;
+
+public class ImageModule implements CarModule {
+    @Override
+    public void update() {
+        System.out.println("Image3");
+    }
+}
---
+++ speed/src/main/java/org.avphs.speed/org/avphs/speed/SpeedModule.java
@@ -0,0 +1,10 @@
+package org.avphs.speed;
+
+import org.avphs.core.CarModule;
+
+public class SpeedModule implements CarModule {
+    @Override
+    public void update() {
+        System.out.println("Speed5");
+    }
+}
