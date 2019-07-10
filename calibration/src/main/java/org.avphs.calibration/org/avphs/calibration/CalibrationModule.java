c9c7c4e16d3a677f912caceeab424c693bfe290e
StudentSydney <sydv793@gmail.com>
2019-07-10 14:51:43 -0700

    add documentation

--- calibration/src/main/java/org.avphs.calibration/org/avphs/calibration/CalibrationModule.java
+++ calibration/src/main/java/org.avphs.calibration/org/avphs/calibration/CalibrationModule.java
@@ -6,8 +6,10 @@ import org.avphs.core.CarModule;
 
 public class CalibrationModule {
 
+    //input current speed and desired speed. get distance
     public static final byte[][] SPEED_CHANGE_DISTS = {};
 
+    //input floor type, radius of turn, get max velocity
     public static final byte[][] MAX_SPEEDS = {};
 
     //input x and y
