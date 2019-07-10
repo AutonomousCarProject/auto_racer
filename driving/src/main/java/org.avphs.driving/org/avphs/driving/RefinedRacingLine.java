c5d8fd657161b5bf5337390417babfc6fa67ffaf
ClemenceYi <33429380+ClemenceYi@users.noreply.github.com>
2019-07-09 23:09:15 -0700

    Making sure everything is ok 	modified:   driving/src/main/java/org.avphs.driving/org/avphs/driving/RefinedRacingLine.java

--- driving/src/main/java/org.avphs.driving/org/avphs/driving/RefinedRacingLine.java
+++ driving/src/main/java/org.avphs.driving/org/avphs/driving/RefinedRacingLine.java
@@ -1,4 +1,24 @@
 package org.avphs.driving;
 
+import org.avphs.util.VectorPoint;
+
+import java.util.*;
+
 public class RefinedRacingLine {
+    private ArrayList<RoadData> roadData;
+    private VectorPoint currentPos;
+
+    public RefinedRacingLine(ArrayList<RoadData> input, VectorPoint currentPos){
+        this.currentPos = currentPos;
+        roadData = input;
+    }
+
+    public ArrayList<RoadData> getRoadData(){
+        return roadData;
+    }
+
+    public VectorPoint getCurrentPos(){
+        return currentPos;
+    }
+
 }
