f0577a7787ab6b6185c80cfcdc693029556cefc7
speedycatfish <ymcacolin@gmail.com>
2019-07-10 09:58:29 -0700

    Added floodfill for inner and outer walls

--- racingline/src/main/java/org.avphs.racingline/org/avphs/racingline/RacingLineModule.java
+++ racingline/src/main/java/org.avphs.racingline/org/avphs/racingline/RacingLineModule.java
@@ -8,8 +8,17 @@ import RacingLinePoint;
 
 public class RacingLineModule implements CarModule {
     private RacingLine racingLine;
+    private ArrayList<WallPoint> outerWall = new ArrayList<WallPoint>();
+    private ArrayList<WallPoint> innerWall = new ArrayList<WallPoint>();
+    private boolean[][] map;
+    private boolean[][] visited;
+    boolean addToOuter;
+    int length, width;
+    private int[] dx = {-1, 0, 1, 0};
+    private int[] dy = {0, 1, 0, -1};
 
     @Override
+
     public Class[] getDependencies() {
         return null;
     }
@@ -36,7 +45,10 @@ public class RacingLineModule implements CarModule {
      * @see RacingLine getRacingLine()
      */
     public void makeRacingLine(boolean[][] map) {
-        System.out.println("RacingLine.makeRacingLine not implemented");
+        this.map = map;
+        length = map.length;
+        width = map[0].length;
+        getMiddleLine();
     }
 
     /**
@@ -48,6 +60,45 @@ public class RacingLineModule implements CarModule {
      */
     public RacingLine getRacingLine() {
         System.out.println("RacingLine.getRacingLine not implemented");
+    }
+
+    private void getMiddleLine() {
+        getWalls();
+    }
+
+    private void getWalls() {
+        visited = new boolean[length][width];
+        addToOuter = true;
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < width; j++) {
+                if (map[i][j] == false && visited[i][j] == false) {
+                    DFS(i, j);
+                    addToOuter = false;
+                }
+            }
+        }
+        addToOuter = false;
+    }
+
+    private void DFS(int x, int y) {
+        visited[x][y] = true;
+        for (int i = 0; i < 4; i++) {
+            int tx = x + dx[i];
+            int ty = y + dy[i];
+            if (tx >= 0 && <length && ty >= 0 && ty < width){
+                if (map[tx][ty] == true) {
+                    WallPoint newPoint = new WallPoint(tx, ty);
+                    if (addToOuter == true) {
+                        outerWall.add(newPoint);
+                    } else {
+                        innerWall.add(newPoint);
+                    }
+                }
+                if (visited[tx][ty] == false) {
+                    DFS(tx, ty);
+                }
+            }
+        }
     }
 }
 
@@ -114,4 +165,13 @@ public class RacingLinePoint {
         this.degree = degree;
     }
 
+}
+
+private class WallPoint {
+    int x, y;
+
+    public WallPoint(int _x, int _y) {
+        x = _x;
+        y = _y;
+    }
 }
