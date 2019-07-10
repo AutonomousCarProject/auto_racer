a1b9c0081e40e1ef5cf48bb4db6052562b14362c
ClemenceYi <33429380+ClemenceYi@users.noreply.github.com>
2019-07-10 10:00:04 -0700

    parent 3bb9d2d029d6bc5e3d6d7e45c0ad40a04f3e126c author ClemenceYi <33429380+ClemenceYi@users.noreply.github.com> 1562778004 -0700 committer ClemenceYi <33429380+ClemenceYi@users.noreply.github.com> 1562865951 -0700
    parent 3bb9d2d029d6bc5e3d6d7e45c0ad40a04f3e126c
    author ClemenceYi <33429380+ClemenceYi@users.noreply.github.com> 1562778004 -0700
    committer ClemenceYi <33429380+ClemenceYi@users.noreply.github.com> 1562865457 -0700
    
    Corrected formula for Straight
    	modified:   driving/src/main/java/org.avphs.driving/org/avphs/driving/Straight.java

--- driving/src/main/java/org.avphs.driving/org/avphs/driving/Speed.java
+++ driving/src/main/java/org.avphs.driving/org/avphs/driving/Speed.java
@@ -1,37 +1,64 @@
 package org.avphs.driving;
 
 
-import org.avphs.util.VectorPoint;
-
 public class Speed {
-    float coefFric;     //get from calibration
-    float forceGrav;    //get from calibration
-    int targetSpeed;
-    boolean isStraight;
-    VectorPoint currentPos;
-    private RoadData roadData;
-    public Speed(RoadData input, VectorPoint currentPos){
-        roadData = input;
+    /* Currently, Speed will work like this:
+    * The constructor takes your current position, current segment, and next upcoming segment.
+    * Each frame, getThrottle will return the throttle that the wheels will run at.
+    * Each time the car moves to a new segment, newSegment will run, setting the next segment up.
+    */
+
+    //private int targetSpeed;
+    //private boolean isStraight;
+    private int speedChange;    //negative = slow down, positive = speed up
+    private int brakeDist;
+    private final int MAX_SPEED;
+    private final int MAX_HARD_BRAKE; //max throttle for braking w/o skidding
+    private VectorPoint currentPos;
+    private RoadData currentSegment;
+    private RoadData nextSegment;
+
+    public Speed(int MAX_SPEEDin, int MAX_HARD_BRAKEin, VectorPoint currentPos, RoadData currentSegment, RoadData nextSegment){
+        this.MAX_SPEED = MAX_SPEEDin;
+        this.MAX_HARD_BRAKE = MAX_HARD_BRAKEin;
         this.currentPos = currentPos;
+        this.currentSegment = currentSegment;
+        this.nextSegment = nextSegment;
+
+        brakeDist = 2;
+    }
+
+    public void setCurrentPos(VectorPoint newCurrentPos){
+        currentPos = newCurrentPos;
+    }
+
+    public void newSegment(RoadData newNextSeg){
+        currentSegment = nextSegment;
+        nextSegment = newNextSeg;
+        speedChange = getTargetSpeedSegment(nextSegment) - getTargetSpeedSegment(currentSegment);
+        brakeDist = 2;
     }
 
     public int getThrottle(){
+        if (currentSegment instanceof Straight){
+            if (true){
+                return 180;
+            } else {
+                return MAX_HARD_BRAKE;
+            }
 
-        if (isStraight){
-            targetSpeed = getTargetSpeedForStraight((Straight)roadData);
-            float forceFric = -1*forceGrav*coefFric;
+//            return 180;     //currently returning max throttle
         } else {
-            targetSpeed = getTargetSpeedForTurn((Turn)roadData);
-
+            return 90;
         }
-        return 1;
     }
 
-    private int getTargetSpeedForStraight(Straight input){
-        return 1;
+    private int getTargetSpeedSegment(RoadData input){
+        if (input instanceof Straight){
+            return MAX_SPEED;
+        } else {
+            return 4;   //dummy value for now
+        }
     }
-
-    private int getTargetSpeedForTurn(Turn input) { return 1; }
-
 }
 
