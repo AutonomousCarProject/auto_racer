c62676608304039d2af176761caaf841babee9a7
P A <pandanautinspace@gmail.com>
2019-07-10 13:10:41 -0700

    Add basic posterization method. Will reorganize later. Removed unnecessary blurring.

--- image/src/main/java/org.avphs.image/org/avphs/image/ImageProcessing.java
+++ image/src/main/java/org.avphs.image/org/avphs/image/ImageProcessing.java
@@ -1,3 +1,5 @@
+package org.avphs.image;
+
 interface ImageProcessingInterface {
     int[] getWallHeights();
     int[] getWallTypes();
@@ -27,11 +29,72 @@ public class ImageProcessing implements ImageProcessingInterface {
 
     }
 
-    void blurImage() {
-
+    static int getRed(int rgb) {
+        return (rgb >> 16) & 0xFF;
     }
 
-    void posterizeImage() {
+    static int getGreen(int rgb) {
+        return (rgb >> 8) & 0xFF;
+    }
 
+    static int getBlue(int rgb) {
+        return rgb & 0xFF;
+    }
+
+    static int combineRGB(int red, int green, int blue) {
+        return blue + (green << 8) + (red << 16);
+    }
+
+    static void posterizeImage(int[] rgbArray, int[] outArray, int diffThreshold) {
+        int dt = diffThreshold;
+        for(int i = rgbArray.length - 1; i >= 0; i --) {
+            int current = rgbArray[i];
+            int red = getRed(current);
+            int green = getGreen(current);
+            int blue = getBlue(current);
+            int rg = red - green;
+            int rb = red - blue;
+            int bg = blue - green;
+            if(rg > dt && rb > dt) {
+                outArray[i] = combineRGB(255,0,0);
+            }
+            else if (rg < -dt && bg < -dt) {
+                outArray[i] = combineRGB(0,255,0);
+            }
+            else if (rb < -dt && bg > dt) {
+                outArray[i] = combineRGB(0,0,255);
+            }
+            else if (rg < dt && rg > -dt && rb > dt && bg < -dt) {
+                outArray[i] = combineRGB(255,255,0);
+            }
+            else if (rb < dt && rb > -dt && rg > dt && bg > dt) {
+                outArray[i] = combineRGB(255,0,255);
+            }
+            else if (bg < dt && bg > -dt && rg < -dt && rb < -dt ) {
+                outArray[i] = combineRGB(0,255,255);
+            }
+            else {
+                int avg = (int)((red + green + blue) * 0.333);
+                if(avg < 25) {
+                    outArray[i] = combineRGB(0,0,0);
+                }
+                else if(avg < 76) {
+                    outArray[i] = combineRGB(51,51,51);
+                }
+                else if(avg < 127) {
+                    outArray[i] = combineRGB(102,102,102);
+                }
+                else if(avg < 178) {
+                    outArray[i] = combineRGB(153,153,153);
+                }
+                else if(avg < 229) {
+                    outArray[i] = combineRGB(204,204,204);
+                }
+                else {
+                    outArray[i] = combineRGB(255,255,255);
+                }
+
+            }
+        }
     }
 }
