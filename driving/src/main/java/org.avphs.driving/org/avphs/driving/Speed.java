f72177adfe785f6cfb1fbf18ff4d5a9afd5cc907
ClemenceYi <33429380+ClemenceYi@users.noreply.github.com>
2019-07-10 12:36:04 -0700

    Fixed merge errors 	modified:   driving/src/main/java/org.avphs.driving/org/avphs/driving/Speed.java

--- driving/src/main/java/org.avphs.driving/org/avphs/driving/Speed.java
+++ driving/src/main/java/org.avphs.driving/org/avphs/driving/Speed.java
@@ -4,13 +4,6 @@ package org.avphs.driving;
 import org.avphs.util.VectorPoint;
 
 public class Speed {
-<<<<<<< HEAD
-    private int targetSpeed;
-    private boolean isStraight;
-    private VectorPoint currentPos;
-    private RefinedRacingLine roadData;
-    public Speed(RefinedRacingLine input, VectorPoint currentPos){
-=======
     //private int targetSpeed;
     //private boolean isStraight;
     private int speedChange;    //negative = slow down, positive = speed up
@@ -20,7 +13,6 @@ public class Speed {
     private RoadData currentSegment;
     private RoadData nextSegment;
     public Speed(RefinedRacingLine input, VectorPoint currentPos, RoadData currentSegment, RoadData nextSegment){
->>>>>>> 86de5f9798af01d463fc6aeb10b4ab65c9d3d0e4
         roadData = input;
         this.currentPos = currentPos;
         this.currentSegment = currentSegment;
@@ -31,14 +23,6 @@ public class Speed {
         /*if (isStraight){
             //targetSpeed = getTargetSpeedForStraight((Straight)roadData);
             targetSpeed = max_speed;
-
-<<<<<<< HEAD
-        if (isStraight){
-            //targetSpeed = getTargetSpeedForStraight((Straight)roadData);
-        } else {
-            //targetSpeed = getTargetSpeedForTurn((Turn)roadData);
-=======
-
             return 180;     //currently returning max throttle
         } else {
             //targetSpeed = getTargetSpeedForTurn((Turn)roadData);
@@ -47,7 +31,6 @@ public class Speed {
         speedChange = getTargetSpeedSegment(nextSegment) - getTargetSpeedSegment(currentSegment);
         return 180;     //returns max throttle for now
     }
->>>>>>> 86de5f9798af01d463fc6aeb10b4ab65c9d3d0e4
 
     private int getTargetSpeedSegment(RoadData input){
         if (input instanceof Straight){
@@ -60,13 +43,5 @@ public class Speed {
     private int getTargetSpeedNextSegment(RoadData input) {
         return 1;
     }
-
-<<<<<<< HEAD
-    private int getTargetSpeedForTurn(Turn input) {
-        return 1;
-    }
-
-=======
->>>>>>> 86de5f9798af01d463fc6aeb10b4ab65c9d3d0e4
 }
 
