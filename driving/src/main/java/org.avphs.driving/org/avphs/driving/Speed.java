86de5f9798af01d463fc6aeb10b4ab65c9d3d0e4
Daquartzinator <jasper.bushnell@gmail.com>
2019-07-10 11:17:19 -0700

    Started getting throttle on straights

--- driving/src/main/java/org.avphs.driving/org/avphs/driving/Speed.java
+++ driving/src/main/java/org.avphs.driving/org/avphs/driving/Speed.java
@@ -4,31 +4,45 @@ package org.avphs.driving;
 import org.avphs.util.VectorPoint;
 
 public class Speed {
-    private int targetSpeed;
-    private boolean isStraight;
+    //private int targetSpeed;
+    //private boolean isStraight;
+    private int speedChange;    //negative = slow down, positive = speed up
+    private int max_speed;
     private VectorPoint currentPos;
     private RefinedRacingLine roadData;
-    public Speed(RefinedRacingLine input, VectorPoint currentPos){
+    private RoadData currentSegment;
+    private RoadData nextSegment;
+    public Speed(RefinedRacingLine input, VectorPoint currentPos, RoadData currentSegment, RoadData nextSegment){
         roadData = input;
         this.currentPos = currentPos;
+        this.currentSegment = currentSegment;
+        this.nextSegment = nextSegment;
     }
 
     public int getThrottle(){
-
-        if (isStraight){
+        /*if (isStraight){
             //targetSpeed = getTargetSpeedForStraight((Straight)roadData);
+            targetSpeed = max_speed;
+
+
+            return 180;     //currently returning max throttle
         } else {
             //targetSpeed = getTargetSpeedForTurn((Turn)roadData);
+            return 1;
+        }*/
+        speedChange = getTargetSpeedSegment(nextSegment) - getTargetSpeedSegment(currentSegment);
+        return 180;     //returns max throttle for now
+    }
 
+    private int getTargetSpeedSegment(RoadData input){
+        if (input instanceof Straight){
+            return max_speed;
+        } else {
+            return 0;
         }
-        return 1;
     }
 
-    private int getTargetSpeedForStraight(Straight input){
-        return 1;
-    }
-
-    private int getTargetSpeedForTurn(Turn input) {
+    private int getTargetSpeedNextSegment(RoadData input) {
         return 1;
     }
 
