518b99c36d37643b40b0f50944be1c4b9057b232
Michael Schuff <>
2019-07-11 09:45:14 -0700

    getPosition now returns VectorPoint, added function getPosDirSpeed to return position direction and speed all at once

--- position/src/main/java/org.avphs.position/module-info.java
+++ position/src/main/java/org.avphs.position/module-info.java
@@ -2,6 +2,7 @@ module org.avphs.position {
     requires org.avphs.core;
     requires org.avphs.map;
     requires org.avphs.image;
+
     exports org.avphs.position;
 
     provides org.avphs.core.CarModule with org.avphs.position.PositionModule;
--- position/src/main/java/org.avphs.position/org/avphs/position/PositionModule.java
+++ position/src/main/java/org.avphs.position/org/avphs/position/PositionModule.java
@@ -4,16 +4,20 @@ import org.avphs.core.CarCommand;
 import org.avphs.core.CarModule;
 import org.avphs.image.ImageModule;
 import org.avphs.map.MapModule;
+import org.avphs.util.VectorPoint;
 
 public class PositionModule implements CarModule {
 
     private ImageModule imageModule;
     private MapModule mapModule;
+    private float[] position = {0,0};
+    private float direction=0;
+    private float speed=0;
 
     @Override
     public Class[] getDependencies() {
         return new Class[] {
-        	ImageModule.class, MapModule.class
+        	ImageModule.class, MapModule.class, VectorPoint.class
         };
     }
 
@@ -37,16 +41,20 @@ public class PositionModule implements CarModule {
     
     
     //THE CODE BELOW IS TEMPORARY AND SUBJECT TO CHANGE PROBABLY VERY SOON
-    public float[] getPosition(){ //returns the (x,y) of the car (ideally synced with the map)
-        return new float[]{0,0};
+    public VectorPoint getPosition(){ //returns the VectorPoint(x,y) of the car (ideally synced with the map)
+        return new VectorPoint(position[0],position[1]);
     }
     
     public float getDirection(){ //returns the direction of the car in degrees, always 0<= x <360
-        return 0.0;
+        return (float) 0;
     }
-    
+
+    public float[] getPosDirSpeed(){
+        return new float[]{position[0], position[1], direction, speed};
+    }
+
     //DEPRECATED BECAUSE THIS EXISTS IN TRAKSIM
-    public float getSpeed(){
-        return 0.0;
+    public float getSpeed() {
+        return (float) 0;
     }
 }
