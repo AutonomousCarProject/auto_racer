95122772aa262f48250cf493894668841a2a49b7
Daquartzinator <jasper.bushnell@gmail.com>
2019-07-11 14:55:15 -0700

    Changed privates in RoadData to Protected

--- driving/src/main/java/org.avphs.driving/org/avphs/driving/RoadData.java
+++ driving/src/main/java/org.avphs.driving/org/avphs/driving/RoadData.java
@@ -1,11 +1,11 @@
 package org.avphs.driving;
 
 public abstract class RoadData {
-    private float startX = 0;
-    private float startY = 0;
-    private float endX = 0;
-    private float endY = 0;
-    private short radius = 0;
+    protected float startX;
+    protected float startY;
+    protected float endX;
+    protected float endY;
+    protected short radius;
 
     public short getRadius(){
         return radius;
