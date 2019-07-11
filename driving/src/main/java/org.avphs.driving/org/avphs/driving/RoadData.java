36b67d4e0bbc0625eecb3ef89f459faa39fbe5d6
ClemenceYi <33429380+ClemenceYi@users.noreply.github.com>
2019-07-11 15:46:03 -0700

    Adding real declaration for constants
    	modified:   driving/src/main/java/org.avphs.driving/org/avphs/driving/RoadData.java

--- driving/src/main/java/org.avphs.driving/org/avphs/driving/RoadData.java
+++ driving/src/main/java/org.avphs.driving/org/avphs/driving/RoadData.java
@@ -1,11 +1,11 @@
 package org.avphs.driving;
 
 public abstract class RoadData {
-    float startX = 0;
-    float startY = 0;
-    float endX = 0;
-    float endY = 0;
-    short radius = 0;
+    protected float startX;
+    protected float startY;
+    protected float endX;
+    protected float endY;
+    protected short radius;
 
     public short getRadius(){
         return radius;
