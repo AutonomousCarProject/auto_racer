1bd5233146399cb59b8109d5d6269aaa77ba9dad
WHS-RB <Moquito35wow@gmail.com>
2019-07-11 09:20:44 -0700

    deleted calibration constants class

--- calibration/src/main/java/org.avphs.calibration/org/avphs/calibration/Calib_consts.java
+++
@@ -1,5 +0,0 @@
-package org.avphs.calibration;
-public class Calib_consts;
-public static final float
-C_MetersPerTurn = 0.15; //Meters per turn, 1.2 in park meters (1:8 scale)
-//This list will grow
