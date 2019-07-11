271ef7b47e45a37347bdd2c8682d83ea7fe8d59e
Daquartzinator <jasper.bushnell@gmail.com>
2019-07-11 14:09:18 -0700

    # This is a combination of 3 commits. # This is the 1st commit message:
    Adding real declaration for constants
    
    # The commit message #2 will be skipped:
    
    # Adding real declaration for constants
    #
    #	modified:   driving/src/main/java/org.avphs.driving/org/avphs/driving/Speed.java
    #	modified:   driving/src/main/java/org.avphs.driving/org/avphs/driving/Straight.java
    
    # The commit message #3 will be skipped:
    
    # Adding real declaration for constants

--- driving/src/main/java/org.avphs.driving/org/avphs/driving/Straight.java
+++ driving/src/main/java/org.avphs.driving/org/avphs/driving/Straight.java
@@ -1,20 +1,21 @@
 package org.avphs.driving;
 
 
-public class Straight implements RoadData {
-	float straightLen;
-	private float startX;
-	private float startY;
-	private float endX;
-	private float endY;
+public class Straight extends RoadData {
+	private float straightLen;
 
 	public Straight(float startX, float startY, float endX, float endY) {
 		this.startX = startX;
 		this.startY = startY;
 		this.endX = endX;
 		this.endY = endY;
+		this.radius = 0;
 
 		straightLen = (float)Math.sqrt(Math.pow(endX - startX, 2.0) + Math.pow(endY - startY, 2.0));
+	}
+
+	public float getStraightLen(){
+		return straightLen;
 	}
 }
 
