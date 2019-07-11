bf3b1c0b344c8b7b47e91b43aebe700875a46576
ClemenceYi <33429380+ClemenceYi@users.noreply.github.com>
2019-07-11 09:57:41 -0700

    Changed Steering and DrivingModule to work with information updates 	modified:   driving/src/main/java/org.avphs.driving/org/avphs/driving/DrivingModule.java 	modified:   driving/src/main/java/org.avphs.driving/org/avphs/driving/Steering.java

--- driving/src/main/java/org.avphs.driving/org/avphs/driving/DrivingModule.java
+++ driving/src/main/java/org.avphs.driving/org/avphs/driving/DrivingModule.java
@@ -27,25 +27,26 @@ public class DrivingModule implements CarModule {
         System.out.println("Driving");
     }
 
-    private RacingLine racingline;
+    private RacingLine[] racingline;
     private ArrayList<RoadData> roadData;
     private RoadData currentSegment;
     private RoadData nextSegment;
     private int angle = 90;
     private int throttle = 90;
     private VectorPoint currentPos;
+    Speed speed = new Speed(currentPos, currentSegment, nextSegment);
+    Steering steer = new Steering(currentPos, currentSegment);
 
-    public DrivingModule(RacingLine racingLine){
+    public DrivingModule(RacingLine[] racingLine){
         this.racingline = racingLine;
         roadData = new ArrayList<RoadData>();
         analyzeRacingLine(racingLine);
     }
 
     public DrivingModule(){
-        //This is the stupidest idea, having a "default constructor"
     }
 
-    public void analyzeRacingLine(RacingLine input){
+    public void analyzeRacingLine(RacingLine[] input){
         ArrayList<RacingLinePoint> racingLinePoints = new ArrayList<RacingLinePoint>();
         //racingLine.getRacingLinePoints();
     }
@@ -54,22 +55,16 @@ public class DrivingModule implements CarModule {
         currentPos = input;
     }
 
-
-
     public VectorPoint getPosition(){ //returns the (x,y) of the car in the map
         return null;
     }
     
     public int getDirection(){ //returns the direction of the car from 0 to 180
-        Steering steer = new Steering(currentPos, currentSegment);
-        angle = steer.getAngle();
-        return angle;
+        return angle = steer.getAngle();
     }
 
     public int getThrottle() { //returns the throttle of the car from 0 to 180
-        Speed speed = new Speed(currentPos, currentSegment, nextSegment);
-        throttle = speed.getThrottle();
-        return throttle;
+        return throttle = speed.getThrottle();
     }
 
 }
--- driving/src/main/java/org.avphs.driving/org/avphs/driving/Steering.java
+++ driving/src/main/java/org.avphs.driving/org/avphs/driving/Steering.java
@@ -6,15 +6,22 @@ public class Steering {
     private RoadData currentSegment;
 
 
-    public Steering(VectorPoint currentPos, RoadData currentSegment){
+    public Steering(VectorPoint currentPos, RoadData currentSegment) {
         this.currentSegment = currentSegment;
         this.currentPos = currentPos;
     }
 
+    public void changeCurrentPos(VectorPoint newCurrentPos) {
+        currentPos = newCurrentPos;
+    }
+
+    public void changeCurrentSegment(RoadData newCurrentSegment){
+        currentSegment = newCurrentSegment;
+    }
 
     public int getAngle(){
         if (currentSegment instanceof Straight){
-
+            
         } else {
 
         }
