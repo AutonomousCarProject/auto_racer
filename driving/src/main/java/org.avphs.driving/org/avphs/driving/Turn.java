5501df9fca8dbec743908c2f658df0596bbd54b2
Daquartzinator <jasper.bushnell@gmail.com>
2019-07-11 14:34:01 -0700

    Updating vars, consts, and class types and arguments in Speed, Straight, and Turn. 	modified:   driving/src/main/java/org.avphs.driving/org/avphs/driving/RoadData.java 	modified:   driving/src/main/java/org.avphs.driving/org/avphs/driving/Speed.java 	modified:   driving/src/main/java/org.avphs.driving/org/avphs/driving/Straight.java 	modified:   driving/src/main/java/org.avphs.driving/org/avphs/driving/Turn.java

--- driving/src/main/java/org.avphs.driving/org/avphs/driving/Turn.java
+++ driving/src/main/java/org.avphs.driving/org/avphs/driving/Turn.java
@@ -1,16 +1,9 @@
 package org.avphs.driving;
 
-public class Turn implements RoadData{
-	private float radius;
-	private float angle;
-	private float startX;
-	private float startY;
-	private float endX;
-	private float endY;
+public class Turn extends RoadData{
 
-	public Turn(float startX, float startY, float endX, float endY, float radius, float angle) {
+	public Turn(float startX, float startY, float endX, float endY, short radius) {
 		this.radius = radius;
-		this.angle = angle;
 		this.startX = startX;
 		this.startY = startY;
 		this.endX = endX;
