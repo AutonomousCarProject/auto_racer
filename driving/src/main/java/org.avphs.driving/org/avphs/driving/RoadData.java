4b917e5aa3dac790ae4eff388a0d543ba92cf91c
Daquartzinator <jasper.bushnell@gmail.com>
2019-07-11 14:37:47 -0700

    Fixed error: RoadData arguments are now private

--- driving/src/main/java/org.avphs.driving/org/avphs/driving/RoadData.java
+++ driving/src/main/java/org.avphs.driving/org/avphs/driving/RoadData.java
@@ -1,11 +1,11 @@
 package org.avphs.driving;
 
 public abstract class RoadData {
-    float startX = 0;
-    float startY = 0;
-    float endX = 0;
-    float endY = 0;
-    short radius = 0;
+    private float startX = 0;
+    private float startY = 0;
+    private float endX = 0;
+    private float endY = 0;
+    private short radius = 0;
 
     public short getRadius(){
         return radius;
