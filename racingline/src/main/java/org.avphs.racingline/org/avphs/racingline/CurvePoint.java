9e6ad77245b9493393724837cf35bc2063f82b6d
Jonathan Guo <jonguo6@gmail.com>
2019-07-16 11:08:03 -0700

    updated curve point class

--- racingline/src/main/java/org.avphs.racingline/org/avphs/racingline/CurvePoint.java
+++ racingline/src/main/java/org.avphs.racingline/org/avphs/racingline/CurvePoint.java
@@ -1,21 +1,21 @@
 package org.avphs.racingline;
 
 public class CurvePoint {
-    int x,y;
-    CurvePoint next;
-    CurvePoint previous;
-    float pdx,pdy;
-    float ndx,ndy;
-    float tdx,tdy;
+    private int x,y;
+    private CurvePoint next;
+    private CurvePoint previous;
+    private float pdx,pdy;
+    private float ndx,ndy;
+    private float tdx,tdy;
     public CurvePoint(int a, int b) {
         x = a;
         y = b;
     }
     public void start() {
-        pdx = x - previous.x;
-        pdy = y - previous.y;
-        ndx = next.x - x;
-        ndy = next.y - y;
+        pdx = x - previous.getX();
+        pdy = y - previous.getY();
+        ndx = next.getX() - x;
+        ndy = next.getY() - y;
         float[] pdn = normalise(pdx,pdy);
         float[] ndn = normalise(ndx,ndy);
         tdx = (pdn[0]+ndn[0])/2;
@@ -23,7 +23,6 @@ public class CurvePoint {
         float[] tdn = normalise(tdx,tdy);
         tdx = tdn[0];
         tdy = tdn[1];
-        //test
     }
     public float magnitude(float a, float b) {
         return (float)Math.sqrt(a*a+b*b);
@@ -31,5 +30,36 @@ public class CurvePoint {
     public float[] normalise(float a, float b) {
         float magn = magnitude(a,b);
         return new float[] {a/magn,b/magn};
+    }
+
+    public int getX() {
+        return x;
+    }
+    public int getY() {
+        return y;
+    }
+    public float getTargetX() {
+        return tdx;
+    }
+    public float getTargetY() {
+        return tdy;
+    }
+    public CurvePoint getNext() {
+        return next;
+    }
+    public CurvePoint getPrevious() {
+        return previous;
+    }
+    public void setX(int a) {
+        x = a;
+    }
+    public void setY(int a) {
+        y = a;
+    }
+    public void setNext(CurvePoint a) {
+        next = a;
+    }
+    public void setPrevious(CurvePoint a) {
+        previous = a;
     }
 }
