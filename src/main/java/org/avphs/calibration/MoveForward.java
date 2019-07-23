cbf3209ad8796becd0144db5875b4ad4be98b4a3
Daquartzinator <jasper.bushnell@gmail.com>
2019-07-23 14:41:42 -0700

    MoveForward will output to a file (will work once Interpolater is finished.)

--- src/main/java/org/avphs/calibration/MoveForward.java
+++ src/main/java/org/avphs/calibration/MoveForward.java
@@ -8,6 +8,9 @@ import org.avphs.coreinterface.CarData;
 import org.avphs.image.ImageData;
 import org.avphs.sbcio.ArduinoData;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 
 public class MoveForward {
@@ -35,22 +38,46 @@ public class MoveForward {
 
         car.accelerate(true, 10);
         car.steer(true, 0);
-        while(true){
-            data = (ArduinoData)carData.getModuleData("arduino");
-            dist = data.count;
-            wallHeights.add(imageData.wallBottom[320] - imageData.wallTop[320]);
-            distances.add(dist);
-            if(dist < 1){
-                break;
+
+        try (PrintWriter writer = new PrintWriter("C:\\Users\\daqua\\Documents\\Code\\Sourcetree\\NWAPW_racing\\src\\main\\java\\org\\avphs\\calibration\\PixelData.txt",
+                StandardCharsets.UTF_8)) {
+            while(true){
+                data = (ArduinoData)carData.getModuleData("arduino");
+                dist = data.count;
+                wallHeights.add(imageData.wallBottom[320] - imageData.wallTop[320]);
+                distances.add(dist);
+                if(dist < 1){
+                    break;
+                }
+            }
+            car.stop();
+
+            writer.println(wallHeights.size());
+            writer.print("\n");
+            Interpolater interpolater = new Interpolater(distances, wallHeights);
+            for(int i = 0; i < wallHeights.size(); i++){
+                writer.print(' ');
+                writer.print(Interpolater.getY(i));
+            }
+        } catch(IOException e) {
+            while(true){
+                data = (ArduinoData)carData.getModuleData("arduino");
+                dist = data.count;
+                wallHeights.add(imageData.wallBottom[320] - imageData.wallTop[320]);
+                distances.add(dist);
+                if(dist < 1){
+                    break;
+                }
+            }
+            car.stop();
+
+            for(int i = 0; i < wallHeights.size(); i++){
+                System.out.print("Wall Heights:\n");
+                System.out.print(wallHeights);
+                System.out.print("\n\nDistances from wall:\n");
+                System.out.print(distances);
             }
         }
-        car.stop();
 
-        for(int i = 0; i < wallHeights.size(); i++){
-            System.out.print("Wall Heights:\n");
-            System.out.print(wallHeights);
-            System.out.print("\n\nDistances from wall:\n");
-            System.out.print(distances);
-        }
     }
 }
