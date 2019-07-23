5e4fb48b5cbd7e826f7d8c148228f7cb35aadf25
Jonathan Guo <jonguo6@gmail.com>
2019-07-23 13:51:54 -0700

    passing init

---
+++ src/main/java/org/avphs/passing/PassingModule.java
@@ -0,0 +1,21 @@
+package org.avphs.passing;
+
+import org.avphs.coreinterface.CarCommand;
+import org.avphs.coreinterface.CarData;
+import org.avphs.coreinterface.CarModule;
+
+public class PassingModule implements CarModule {
+    public void init(CarData carData) {
+        System.out.println("Passing init");
+    }
+
+    @Override
+    public CarCommand[] commands() {
+        return null;
+    }
+
+    @Override
+    public void update(CarData carData) {
+        System.out.println("Passing update");
+    }
+}
