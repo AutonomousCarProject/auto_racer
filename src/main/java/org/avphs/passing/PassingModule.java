23631b7b9880ed7fcd7738f35a0cf9bc9a9a3da6
Adam Gallina <34255244+Sir-Aulen@users.noreply.github.com>
2019-07-23 13:55:20 -0700

    Added code regions

--- src/main/java/org/avphs/passing/PassingModule.java
+++ src/main/java/org/avphs/passing/PassingModule.java
@@ -5,6 +5,7 @@ import org.avphs.coreinterface.CarData;
 import org.avphs.coreinterface.CarModule;
 
 public class PassingModule implements CarModule {
+    //region Overrides
     public void init(CarData carData) {
         System.out.println("Passing init");
     }
@@ -18,8 +19,11 @@ public class PassingModule implements CarModule {
     public void update(CarData carData) {
         System.out.println("Passing update");
     }
+    //endregion
 
+    //region Constructors
     public PassingModule() {
 
     }
+    //endregion
 }
