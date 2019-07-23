96320a7da86bbe99a653cdd8aa15094693579bab
Michael Schuff <>
2019-07-23 11:13:58 -0700

    print

--- src/main/java/org/avphs/position/PositionModule.java
+++ src/main/java/org/avphs/position/PositionModule.java
@@ -86,6 +86,7 @@ public class PositionModule implements CarModule {
 
         //THIS WILL BE USED LATER
         prevPositionData.updateAll(positionData.getPosition(), positionData.getDirection(), positionData.getSpeed());
+        System.out.println("Position = ("+Math.round(positionData.getPosition()[0])+","+positionData.getPosition()[1]+")");
 
     }
 
