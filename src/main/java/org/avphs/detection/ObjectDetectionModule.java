67910690bfa20873d2a420a85b69b6559f2196c4
Gavith <GavinLesher5501@gmail.com>
2019-07-24 13:17:10 -0700

    Module use in object detection, added map data export in init

--- src/main/java/org/avphs/detection/ObjectDetectionModule.java
+++ src/main/java/org/avphs/detection/ObjectDetectionModule.java
@@ -3,22 +3,32 @@ package org.avphs.detection;
 import org.avphs.coreinterface.CarCommand;
 import org.avphs.coreinterface.CarData;
 import org.avphs.coreinterface.CarModule;
+import org.avphs.image.ImageData;
+import org.avphs.map.Map;
+import org.avphs.position.PositionData;
 
 public class ObjectDetectionModule implements CarModule {
     ObjectData data = new ObjectData();
+    private ImageData imageData;
+    private Map map;
+    private PositionData positionData;
+
     @Override
     public void init(CarData carData) {
-
+        carData.addData("objectDetection", data);
     }
 
     @Override
     public CarCommand[] commands() {
         return new CarCommand[0];
-
     }
 
     @Override
     public void update(CarData carData) {
+        imageData = (ImageData)carData.getModuleData("image");
+        map = (Map)carData.getModuleData("map");
+        positionData = (PositionData)carData.getModuleData("position");
+
         carData.addData("objectDetection", data);
     }
 }
