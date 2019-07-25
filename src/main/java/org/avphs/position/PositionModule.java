7e9d36adb76a044b2fae7c2d4a46df5c5897c4a6
Michael Schuff <unknown>
2019-07-25 11:47:44 -0700

    fixed one thing

--- src/main/java/org/avphs/position/PositionModule.java
+++ src/main/java/org/avphs/position/PositionModule.java
@@ -46,7 +46,6 @@ public class PositionModule implements CarModule, CloseHook {
         int steer = (int) carData.getModuleData("driving");
         computePosition(odom.getOdomCount(), steer);
         carData.addData("position", positionData);
-        System.out.println("Position END");
 
 
         //FOR CAR TESTING
@@ -118,11 +117,7 @@ public class PositionModule implements CarModule, CloseHook {
 
     private void convertPosition(float x, float y) {
         //FIXME x and y are currently in cm, not in the virtual world coordinates.
-<<<<<<< HEAD
         if(!(x == 0 && y == 0)){
-=======
-        if(x != 0) {
->>>>>>> yeet
             float[] temp = pol(x, y);
             temp = cart(temp[0], temp[1] - positionData.getDirection());
             positionData.updatePosition(temp);
