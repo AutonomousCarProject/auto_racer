2234f5339c7791cf71aa8992c3cd19b3fc8430ec
StudentSydney <sydv793@gmail.com>
2019-07-26 13:55:35 -0700

    add defishing code

--- src/main/java/org/avphs/core/CalibrationCore.java
+++ src/main/java/org/avphs/core/CalibrationCore.java
@@ -1,9 +1,14 @@
 package org.avphs.core;
 
 import org.avphs.car.Car;
+import org.avphs.coreinterface.CarModule;
 import org.avphs.image.ImageModule;
+import org.avphs.calibration.*;
+
+import java.util.Scanner;
 
 public class CalibrationCore extends CarCore {
+
     public CalibrationCore(Car car, boolean needsCamera) {
         super(car);
 
@@ -11,8 +16,70 @@ public class CalibrationCore extends CarCore {
         if(needsCamera){
             updatingCarModules.add(new ImageModule());
         }
-
-        init();
         startUpdatingModules();
+
+        try {
+            runTime();
+        } catch (Exception e){
+
+        }
+    }
+
+    public void runTime() throws Exception{
+        Scanner sc = new Scanner(System.in);
+        LOOP:
+        while (true){
+            System.out.println("Please input a command");
+            System.out.println("Type 'help' for help");
+            String line = sc.nextLine();
+            switch(line){
+                case "help":
+                    System.out.println("'stop' will stop all functions and close the program");
+                    //System.out.println("'CameraDataGenerator'");
+                    //System.out.println("'3DInterpolation'");
+                    //System.out.println("'MoveForward'");
+                    System.out.println("'ThrottleDataGenerator'");
+                    System.out.println("'BrakeTest'");
+                    System.out.println("'help' prints this little blurb");
+                    break;
+                case "CameraDataGenerator":
+                    updatingCarModules.clear();
+                    //updatingCarModules.add(new CameraDataGenerator());
+                    updatingCarModules.add(car);
+                    updatingCarModules.get(1).init(carData);
+                    updatingCarModules.get(0).init(carData);
+                    break;
+                case "3DInterpolation":
+                    updatingCarModules.clear();
+                    updatingCarModules.add(car);
+                    updatingCarModules.get(1).init(carData);
+                    updatingCarModules.get(0).init(carData);
+                    break;
+                case "MoveForward":
+                    updatingCarModules.clear();
+                    updatingCarModules.add(car);
+                    updatingCarModules.get(1).init(carData);
+                    updatingCarModules.get(0).init(carData);
+                    break;
+                case "BrakeTest":
+                    updatingCarModules.clear();
+                    updatingCarModules.add(new BrakeTest(car));
+                    updatingCarModules.add(car);
+                    updatingCarModules.get(1).init(carData);
+                    updatingCarModules.get(0).init(carData);
+                    break;
+                case "ThrottleDataGenerator":
+                    updatingCarModules.clear();
+                    updatingCarModules.add(new ThrottleDataGenerator(car));
+                    updatingCarModules.add(car);
+                    updatingCarModules.get(1).init(carData);
+                    updatingCarModules.get(0).init(carData);
+                    break;
+                case "stop":
+                    System.out.println("Stopping all calibration functions!");
+                    break LOOP;
+            }
+        }
+        car.stop();
     }
 }
