245955f9b73425fd4b2e984c9d20317a778e45c9
Adam Gallina <34255244+Sir-Aulen@users.noreply.github.com>
2019-07-30 09:41:25 -0700

    Added documentation to all classes except RacingLineModule
    @Jonathan needs to add documentation to Point and CurvePoint; I don't use those functions

--- src/main/java/org/avphs/racingline/CurvePoint.java
+++ src/main/java/org/avphs/racingline/CurvePoint.java
@@ -7,36 +7,15 @@ public class CurvePoint {
     private float pdx,pdy;
     private float ndx,ndy;
     private float tdx,tdy;
+
+    //region Constructors
     public CurvePoint(int a, int b) {
         x = a;
         y = b;
     }
+    //endregion
 
-    /**
-     * Initializes values for CurvePoint.
-     * Should only be called after previous and next have been set.
-     */
-    public void start() {
-        pdx = x - previous.getX();
-        pdy = y - previous.getY();
-        ndx = next.getX() - x;
-        ndy = next.getY() - y;
-        float[] pdn = normalise(pdx,pdy);
-        float[] ndn = normalise(ndx,ndy);
-        tdx = (pdn[0]+ndn[0])/2;
-        tdy = (pdn[1]+ndn[1])/2;
-        float[] tdn = normalise(tdx,tdy);
-        tdx = tdn[0];
-        tdy = tdn[1];
-    }
-    private float magnitude(float a, float b) {
-        return (float)Math.sqrt(a*a+b*b);
-    }
-    private float[] normalise(float a, float b) {
-        float magn = magnitude(a,b);
-        return new float[] {a/magn,b/magn};
-    }
-
+    //region Getters/Setters
     public int getX() {
         return x;
     }
@@ -66,6 +45,33 @@ public class CurvePoint {
     }
     public void setPrevious(CurvePoint a) {
         previous = a;
+    }
+    //endregion
+
+    /**
+     * Initializes values for CurvePoint.
+     * Should only be called after previous and next have been set.
+     */
+    public void start() {
+        pdx = x - previous.getX();
+        pdy = y - previous.getY();
+        ndx = next.getX() - x;
+        ndy = next.getY() - y;
+        float[] pdn = normalise(pdx,pdy);
+        float[] ndn = normalise(ndx,ndy);
+        tdx = (pdn[0]+ndn[0])/2;
+        tdy = (pdn[1]+ndn[1])/2;
+        float[] tdn = normalise(tdx,tdy);
+        tdx = tdn[0];
+        tdy = tdn[1];
+    }
+
+    private float magnitude(float a, float b) {
+        return (float)Math.sqrt(a*a+b*b);
+    }
+    private float[] normalise(float a, float b) {
+        float magn = magnitude(a,b);
+        return new float[] {a/magn,b/magn};
     }
 
     /**
--- src/main/java/org/avphs/racingline/RacingLinePoint.java
+++ src/main/java/org/avphs/racingline/RacingLinePoint.java
@@ -8,6 +8,7 @@ public class RacingLinePoint {
     private boolean pass = false;
     private float passx,passy;
     private boolean original = false;
+
     //region Constructors
     public RacingLinePoint() {
         setX(0);
@@ -27,19 +28,6 @@ public class RacingLinePoint {
         setDegree(degree);
     }
     //endregion
-
-    int crossProductN(RacingLinePoint last, RacingLinePoint next) {
-        float[] vectA = new float[]{getX() - last.getX(), getY() - last.getY()};
-        float[] vectB = new float[]{next.getX() - getX(), next.getY() - getY()};
-
-        return (int) Math.signum(vectA[0] * vectB[1] - vectA[1] * vectB[0]);
-    }
-
-    public float distanceToPoint(RacingLinePoint other) {
-        float diffx = x - other.x;
-        float diffy = y - other.y;
-        return (float) Math.sqrt(diffx * diffx + diffy * diffy);
-    }
 
     //region Getters/Setters
     public float getX() {
@@ -69,34 +57,77 @@ public class RacingLinePoint {
     }
     //endregion
 
+    /**Calculates the Vector Cross Product based on the direction the line is trending in, based on the current point
+     * Used to set the angle between the 3 points as positive or negative
+     *
+     * @param last The previous point in the line
+     * @param next The next point in the line
+     * @return +/-1 based on the Cross product of the three points
+     */
+    int crossProductN(RacingLinePoint last, RacingLinePoint next) {
+        //Convert the points to Vectors
+        float[] vectA = new float[]{getX() - last.getX(), getY() - last.getY()};
+        float[] vectB = new float[]{next.getX() - getX(), next.getY() - getY()};
+
+        //Calculate the cross product, and cast to a +/-1 integer
+        return (int) Math.signum(vectA[0] * vectB[1] - vectA[1] * vectB[0]);
+    }
+
+    /**Returns the distance to another RacingLinePoint (Pythagorean Theorem)
+     *
+     * @param other
+     * @return
+     */
+    public float distanceToPoint(RacingLinePoint other) {
+        float diffx = x - other.x;
+        float diffy = y - other.y;
+        return (float) Math.sqrt(diffx * diffx + diffy * diffy);
+    }
+
     /**
      * Returns a calculated point that is a specified distance towards another point
      * <p>
      * Will return null if other param is the same as the current RacingLinePoint
      *
      * @param other    Give the direction for the calculation
-     * @param distance Distance in map mixels in the given direction
+     * @param distance Distance in map pixels in the given direction
      * @return Return new RacingLinePoint at calculated distance
      */
     public RacingLinePoint distanceTowardsPoint(RacingLinePoint other, float distance) {
+        //Check the the points are not the same
         if (other.equals(this)) {
             System.out.println("RacingLinePoint.distanceTowardsPoint: [@param]other is equal to current point");
             return null;
         }
+
+        //Determine the direction to move the new point in
         float diffx = other.x - x;
         float diffy = other.y - y;
         int dir = diffx < 0 ? -1 : 1;
+        //Get the angle
         float ang = (float) (Math.atan(diffy / diffx));
+
+        //Calculate the new point based on the distance given by the parameter
         float newX = x + (float) (distance * Math.cos(ang) * dir);
         float newY = y + (float) (distance * Math.sin(ang) * dir);
+
+        //Create a RacingLinePoint based on the calculated values
         return new RacingLinePoint(newX, newY);
     }
 
+    /**Convert the Point to a curve point
+     *
+     * @return
+     */
     public CurvePoint toCurvePoint() {
         if(!pass) return new CurvePoint((int)x,(int)y);
         else return new CurvePoint((int)passx,(int)passy);
     }
 
+    /**Returns an (x, y) string of the points coordinates
+     *
+     * @return
+     */
     public String getCoords() {
         return "(" + x + ", " + y + ")";
     }
