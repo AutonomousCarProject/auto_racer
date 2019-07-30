6627cf00cd3e2f1bb82df59da04ba65411c15a5c
StudentSydney <sydv793@gmail.com>
2019-07-30 11:06:43 -0700

    Create CarTests.java

---
+++ src/main/java/org/avphs/calibration/CarTests.java
@@ -0,0 +1,9 @@
+package org.avphs.calibration;
+
+public class CarTests {
+    public static void main(String[] args){
+        short x = 1;
+        byte y = 1;
+        //CalibrationModule.getSpeedChangeDist(x, y, y);
+    }
+}
