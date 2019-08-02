a57ecf402a648776f39cacec1b1aaa55b81befba
StudentSydney <sydv793@gmail.com>
2019-08-01 23:32:18 -0700

    Reverse hash map key

--- src/main/java/org/avphs/calibration/CalibrationModule.java
+++ src/main/java/org/avphs/calibration/CalibrationModule.java
@@ -153,8 +153,8 @@ public class CalibrationModule {
             hashMap = new HashMap<>();
             for (int i = 0; i < radCount; i++) {
                 String[] row = br.readLine().split(" ");
-                int key = Integer.parseInt(row[0]);
-                int val = Integer.parseInt(row[1]);
+                int key = Integer.parseInt(row[1]);
+                int val = Integer.parseInt(row[0]);
                 hashMap.put(key, val);
             }
 
@@ -277,11 +277,11 @@ public class CalibrationModule {
 
     public static final int getAngles(int rad) {
         System.out.println(rad);
-        int stahp = 0;
-        while(!ANGLES.containsKey(rad) && stahp <100){
+        int staaaahp = 0;
+        while(!ANGLES.containsKey(rad) && staaaahp <100){
             rad++;
         }
-        if(stahp>=100){
+        if(staaaahp>=100){
             return STRAIGHT_ANGLE;
         }
         return ANGLES.get(rad);
