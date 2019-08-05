c38046aa3b9cec11ec246db56642eb3369be6703
Kevin <kevindpaulsen@gmail.com>
2019-08-05 13:46:53 -0700

    Fixed errors.

--- src/main/java/org/avphs/driving/newDriving/TurnOptimization.java
+++ src/main/java/org/avphs/driving/newDriving/TurnOptimization.java
@@ -1,13 +1,16 @@
 package org.avphs.driving.newDriving;
 
+import org.avphs.coreinterface.CarData;
+
 public class TurnOptimization implements Drivable {
+
     @Override
-    public int getSteeringAngle() {
+    public int getSteeringAngle(CarData carData) {
         return 0;
     }
 
     @Override
-    public int getThrottle() {
+    public int getThrottle(CarData carData) {
         return 0;
     }
 }
