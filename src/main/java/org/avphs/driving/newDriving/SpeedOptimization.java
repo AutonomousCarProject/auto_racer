ff7721836deb99c1f7092ead9a1e9cd5471cda08
baartmans2 <41588148+baartmans2@users.noreply.github.com>
2019-08-05 12:20:07 -0700

    newDriving added

---
+++ src/main/java/org/avphs/driving/newDriving/SpeedOptimization.java
@@ -0,0 +1,13 @@
+package org.avphs.driving.newDriving;
+
+public class SpeedOptimization implements Driveable {
+    @Override
+    public int getSteeringAngle() {
+        return 0;
+    }
+
+    @Override
+    public int getThrottle() {
+        return 0;
+    }
+}
