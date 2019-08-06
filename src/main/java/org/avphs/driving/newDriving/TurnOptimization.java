426e4d8964843d2d9d89ecf0412c3906019a89b2
baartmans2 <41588148+baartmans2@users.noreply.github.com>
2019-08-06 14:37:03 -0700

    added turndetection class

--- src/main/java/org/avphs/driving/newDriving/TurnOptimization.java
+++ src/main/java/org/avphs/driving/newDriving/TurnOptimization.java
@@ -4,13 +4,19 @@ import org.avphs.coreinterface.CarData;
 
 public class TurnOptimization implements Drivable {
 
+    //TurnOptimization optimizes the driving to make turns as fast as possible.
+
     @Override
-    public int getSteeringAngle(CarData carData) {
+    public int getSteeringAngle(CarData carData)
+    {
         return 0;
     }
 
     @Override
-    public int getThrottle(CarData carData) {
+    public int getThrottle(CarData carData)
+    {
         return 0;
     }
+
+
 }
